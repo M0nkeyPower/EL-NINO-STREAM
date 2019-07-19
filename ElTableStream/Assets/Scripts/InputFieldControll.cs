7c8043c209dcd940219759c7e3e1ba661e66fa7c
M0nkeyPower <ezrvaclav@gmail.com>
2019-07-19 08:57:20 +0200

    Managed to get the script working(still needs work)

---
+++ ElTableStream/Assets/Scripts/InputFieldControll.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputFieldControll : MonoBehaviour {
+
+	 InputField fieldText;
+
+	void Start()
+	{
+		fieldText = gameObject.GetComponent<InputField> ();
+	}
+
+	public void addPoint()
+	{
+		int currentValue = int.Parse (fieldText.text);
+		currentValue++;
+		fieldText.text = currentValue.ToString ();
+	}
+
+
+	public void subtractPoint()
+	{
+		
+	}
+
+
+}
