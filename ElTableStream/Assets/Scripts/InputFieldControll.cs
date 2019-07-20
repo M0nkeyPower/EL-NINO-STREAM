da067aeee46103055896805080502679de78d770
M0nkeyPower <ezrvaclav@gmail.com>
2019-07-20 19:56:50 +0200

    Minor progress

--- ElTableStream/Assets/Scripts/InputFieldControll.cs
+++ ElTableStream/Assets/Scripts/InputFieldControll.cs
@@ -5,25 +5,58 @@ using UnityEngine.UI;
 
 public class InputFieldControll : MonoBehaviour {
 
-	 InputField fieldText;
+	InputField fieldText;
+	public bool autoUpdate;
 
 	void Start()
 	{
 		fieldText = gameObject.GetComponent<InputField> ();
 	}
 
+	//adds 1 to the current value of the inputField
 	public void addPoint()
 	{
+		
 		int currentValue = int.Parse (fieldText.text);
+
+		if (currentValue == null)
+		{
+			fieldText.text = "0";
+
+			return;
+		}
+
 		currentValue++;
+		fieldText.text = currentValue.ToString ();
+
+	}
+
+	//subtracts 1 from the current value of the inputField
+	public void subtractPoint()
+	{
+		int currentValue = int.Parse (fieldText.text);
+
+		if (currentValue == null)
+		{
+			fieldText.text = "0";
+
+			return;
+		}
+
+		currentValue--;
 		fieldText.text = currentValue.ToString ();
 	}
 
 
-	public void subtractPoint()
+	//saves to current value of textField to text file
+	public void update(int currentValue)
 	{
 		
 	}
 
+	public void setAutoUpdate(bool autoUpdate)
+	{
+		
+	}
 
 }
