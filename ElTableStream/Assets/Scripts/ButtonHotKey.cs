2122a694ce8b2c993bb2f339f766ad6b88e47911
M0nkeyPower <ezrvaclav@gmail.com>
2019-07-28 15:03:08 +0200

    Button hotkey script + minor tweaks

---
+++ ElTableStream/Assets/Scripts/ButtonHotKey.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ButtonHotKey : MonoBehaviour {
+
+
+	public List<KeyCode> hotkeys;
+	Button button;
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+		button = gameObject.GetComponent<Button> ();
+
+	}
+	
+
+
+	void OnGUI()
+	{
+		Event e = Event.current;
+		if (e.isKey && !Input.anyKey)
+		{
+			for(int i = 0; i < hotkeys.Count; i++)
+			{
+				if(e.keyCode == hotkeys[i])
+				{
+					button.onClick.Invoke ();
+					break;
+				}
+			}
+		}
+
+
+	}
+}
